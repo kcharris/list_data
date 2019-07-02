09e2b8f26698bc45378f06f92db14894177520b2
kevin <kcharris77@gmail.com>
2019-07-02 10:48:32 -0500

    add app inventory, modify readme, change database to mysql, add inventory URLconf

--- fun_data/settings.py
+++ fun_data/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.inventory',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,7 @@ WSGI_APPLICATION = 'fun_data.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
     }
 }
 
