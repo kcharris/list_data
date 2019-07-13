a3bf2c836d8852f78d99337e4ddfb12e6f6892ea
kevin <kcharris77@gmail.com>
2019-07-12 20:07:01 -0500

    add all songs to view and make available as objects to index

--- inventory/views.py
+++ inventory/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Song, Album, Artist
+
 # Create your views here.
 def index(request):
-  return HttpResponse("Hello, world.")
+  songs = Song.objects.all()
+  return render(request, inventory/index.html, {songs : songs})
