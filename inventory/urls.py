e23bd44f1c97fa1a0f307b291124c7ccfa0d722a
kevin <kcharris77@gmail.com>
2019-07-15 00:22:19 -0500

    add way to create songs on main page and view entire list in database

--- inventory/urls.py
+++ inventory/urls.py
@@ -2,5 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-  path('', views.index, name= 'index')
+  path('', views.index, name= 'index'),
+  path("item_list/", views.item_list, name = "item_list"),
+  path('confirmation/', views.confirmation, name = "confirmation")
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 from .models import Song, Album, Artist
 
 # Create your views here.
 def index(request):
+  return render(request, "inventory/index.html")
+
+def item_list(request):  
   songs = Song.objects.all()
-  return render(request, inventory/index.html, {songs : songs})
+  return render(request, "inventory/item_list.html", {"songs" : songs})
+
+def confirmation(request):
+  form = request.POST
+  b = Song.objects.create(name = form['song'])
+  b.save()
+  return HttpResponseRedirect('../')
