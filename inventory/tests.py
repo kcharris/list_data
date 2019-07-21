921cfa4d94134926b01306f384603db7f5767d27
kevin <kcharris77@gmail.com>
2019-07-20 21:40:04 -0500

    add a simple test case

--- inventory/tests.py
+++ inventory/tests.py
@@ -1,4 +1,13 @@
 from django.test import TestCase
 
-from .models import *
+from .models import Song
 # Create your tests here.
+
+
+class SongModelTests(TestCase):
+    def test_song_was_created(self):
+        """
+        checks that a song was indeed created
+        """
+        song = Song.objects.create(name="cats")
+        self.assertIs(song.name, "cats")
