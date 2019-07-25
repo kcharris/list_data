5636f316ee3dae34d033eec956a19fc6cf316b2c
kevin <kcharris77@gmail.com>
2019-07-25 07:10:26 -0500

    add tests for client post request from confirmation page

--- inventory/tests.py
+++ inventory/tests.py
@@ -1,8 +1,9 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.core.exceptions import ValidationError
 from django.db import IntegrityError
 
 from .models import Song, Album
+from .views import *
 # Create your tests here.
 
 
@@ -19,16 +20,29 @@ class SongModelTests(TestCase):
       """
       with self.assertRaises(IntegrityError):
         song2 = Song.objects.create(name="cats", rating=3)
-    # def test_over_max(self):
-    #   """
-    #   returns error if over max
-    #   """
-    #   with self.assertRaises(ValidationError):
-    #     Song.objects.update
-    # def test_under_min(self):
-    #   "returns error if under min"
-    #   with self.assertRaises(ValidationError):
-    #     song.rating = -1
+        
+    def test_working_client_post(self):
+      """
+      tests to make sure the client is sending the correct responses
+      """
+      c = Client()
+      self.assertRedirects(c.post('/inventory/confirmation/', {'name' : 'seventeen', 'rating': '1'}), "/inventory/")
+
+    def test_over_max(self):
+      """
+      returns error if over max
+      """
+      c = Client()
+      with self.assertRaises(ValueError):
+        c.post('/inventory/confirmation/', {'name' : 'seventeen', 'rating': '101'})
+
+
+    def test_under_min(self):
+      "returns error if under min"
+      c = Client()
+      c.post('/inventory/confirmation/', {'name' : 'seventeen', 'rating': '0'})
+      with self.assertRaises(ValueError):
+        c.post('/inventory/confirmation/', {'name' : 'seventeen', 'rating': '-1'})
 
     def test_song_added_to_album(self):
       song = Song.objects.create(name="burgerdance")
