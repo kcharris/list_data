95fbd55973f7b674c3824b0cc8c81c6507ebdc55
kevin <kcharris77@gmail.com>
2019-08-23 07:42:41 -0500

    remove old directories and copy files

--- inventory/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Song, Artist, Album
-
-# Register your models here.
-@admin.register(Song, Album, Artist)
-class MusicAdmin(admin.ModelAdmin):
-  pass
