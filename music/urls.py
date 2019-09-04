b1d983675c31bacbaf6221d1b245964d688e4e26
kevin <kcharris77@gmail.com>
2019-09-04 05:16:54 -0500

    change all occurrences of music/nav_bar to list_data/nav_bar

--- music/urls.py
+++ music/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 
 
 urlpatterns = [
-  path('music/', views.Index.as_view(), name= 'music'),
-  path("music_list/", views.ItemList.as_view(), name = "music_list"),
+  path('music/', views.Music.as_view(), name= 'music'),
+  path("music_list/", views.Songs.as_view(), name = "music_list"),
   path('confirmation/', views.Confirmation.as_view(), name = 'confirmation'),
   path('accounts_music_list/', views.AccountMusicList.as_view(), name = "accounts_music_list")
 ]
