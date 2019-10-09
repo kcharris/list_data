4e252814a759f9ceba54d4a383cb4b607af845d7
kevin <kcharris77@gmail.com>
2019-10-08 21:44:28 -0500

    update listcreateview to auto-set user to model

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.urls import reverse
+from .views import ListCreateView
+from .models import List
 
 # Create your tests here.
+
+class ListCreateViewTests(TestCase):
+  def setUp(self):
+    User.objects.create_user(username ='testuser', password= 'password')
+    Client().post('/login/', {'username': 'testuser', 'password': 'password'})
+  def test_user_create_list(self):
+    """
+    tests whenever a list is created it is connected to the user that created it
+    """
+    c = Client()
+    c.post('/list/add/', {'name': 'testname', 'users': User.objects.get(username = "testuser")})
+
+    self.assertEqual(List.objects.get(name = 'testname'), List.objects.get(users = 'testuser'))
--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from django.views.generic import ListView, View, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
@@ -13,17 +13,21 @@ class ListCreateView(CreateView):
   model = List
   fields = ["name"]
 
-  success_url = reverse_lazy('list-list')
+  def form_valid(self, form):
+    self.object = form.save()
+    self.object.users.add(self.request.user)
+    self.object.save()
+    return HttpResponseRedirect(self.get_success_url())
 
 class ListUpdateView(UpdateView):
   model = List
   fields = ["name"]
 
-  success_url = reverse_lazy("list-list")
+  success_url = reverse_lazy("lists-list")
 
 class ListDeleteView(DeleteView):
   model = List
-  success_url = reverse_lazy('list-list')
+  success_url = reverse_lazy('lists-list')
 
 class ListDetailView(View):
   def get(self, request, **kwargs):
@@ -32,8 +36,23 @@ class ListDetailView(View):
     items = Item.objects.filter( list = item_list )
     tags = Tag.objects.filter( list = item_list)
     tag_values = ItemTagValue.objects.filter(inventory = item_list)
+    users = item_list.users.all()
     
-    return render(request, "lists/list_detail.html", {'item_list': item_list, 'items':items, 'tags': tags, 'tag_values': tag_values})
+    return render(request, "lists/list_detail.html", {'item_list': item_list, 'items':items, 'tags': tags, 'tag_values': tag_values, 'users': users})
+
+class TagCreateView(CreateView):
+  model = Tag
+  fields = ["name"]
+  success_url = reverse_lazy('lists-list')
+
+class TagUpdateView(UpdateView):
+  model = Tag
+  fields = ['name']
+  success_url = reverse_lazy('lists-list')
+
+class TagDeleteView(DeleteView):
+  model = Tag
+  success_url = reverse_lazy("lists-list")
 
   
 
