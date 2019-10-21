553b21a2cfb4aad956cfd4239491668392ad1897
kevin <kcharris77@gmail.com>
2019-10-21 09:46:49 -0500

    remove old unused directory

--- list/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import ListsList, ListCreate, ListUpdate, ListDelete
-
-urlpatterns = [
-    path('lists/', ListsList.as_view(), name = "lists-list"),
-    path('list/add/', ListCreate.as_view(), name = 'list-add'),
-    path('list/<int:pk>/', ListUpdate.as_view(), name = 'list-update'),
-    path('list/<int:pk>/delete/', ListDelete.as_view(), name = 'list-delete'),
-]
